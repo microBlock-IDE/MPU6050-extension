1441d08d5c5af91e836a98a8538c69a41ae96a74
Max <max30012540@hotmail.com>
2020-09-05 16:51:59 +0700

    Update generators.js

--- blocks/generators.js
+++ blocks/generators.js
@@ -1,13 +1,3 @@
-Blockly.Python['servo'] = function(block) {
-  Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
-  Blockly.Python.definitions_['from_machine_import_pwm'] = 'from machine import PWM';
-
-  var value_pin = Blockly.Python.valueToCode(block, 'pin', Blockly.Python.ORDER_ATOMIC);
-  var value_angle = Blockly.Python.valueToCode(block, 'angle', Blockly.Python.ORDER_ATOMIC);
-  var code = `PWM(Pin(${value_pin}), freq=50).duty(int(25.57 + (((${value_angle}) / 180.0) * 102.3)))\n`;
-  return code;
-};
-
 Blockly.Python['mpu6050_init'] = function(block) {
   Blockly.Python.definitions_['from_machine_import_pin'] = 'from machine import Pin';
   Blockly.Python.definitions_['from_machine_import_i2c'] = 'from machine import I2C';
